cd521622cb21c59a64b8533aef74c993ce513c4b
Jeong Jin Eun <wlsdms0122@gmail.com>
2020-03-29 02:00:51 +0900

    init(): initialize project

---
+++ project_with_spm/Source/Module/App/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  project_with_spm
+//
+//  Created by JSilver on 2020/03/29.
+//  Copyright © 2020 JSilver. All rights reserved.
+//
+
+import UIKit
+import Rxs
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ project_with_spm/Source/Module/Main/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  project_with_spm
+//
+//  Created by JSilver on 2020/03/29.
+//  Copyright © 2020 JSilver. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
